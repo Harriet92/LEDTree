cc9d3929e573e10b2d9287a99d800c54f882e507
Emi <szymanska.emilka@gmail.com>
2017-12-10 14:37:23 +0000

    first draft of a maincontroller script

--- LedController/maincontroller.py
+++ LedController/maincontroller.py
@@ -3,7 +3,7 @@
 #
 # Direct port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
-import time
+import time, sys, json, multiprocessing
 
 from neopixel import *
 
@@ -18,12 +18,25 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 
 
 # Define functions which animate LEDs in various ways.
+def colorWipeSteps(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)   
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
 def colorWipe(strip, color, wait_ms=50):
 	"""Wipe color across display a pixel at a time."""
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, color)   
 	strip.show()
-	time.sleep(wait_ms/1000.0)
+
+def clear(strip):
+	colorWipe(strip, Color(0,0,0))
+
+def wait(s):
+	time.sleep(s)
+	
 
 def theaterChase(strip, color, wait_ms=50, iterations=10):
 	"""Movie theater light style chaser animation."""
@@ -57,11 +70,13 @@ def rainbow(strip, wait_ms=20, iterations=1):
 
 def rainbowCycle(strip, wait_ms=20, iterations=5):
 	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+	print "lol"
+	while True:
+		for j in range(256*iterations):
+			for i in range(strip.numPixels()):
+				strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+			strip.show()
+			time.sleep(wait_ms/1000.0)
 
 def theaterChaseRainbow(strip, wait_ms=50):
 	"""Rainbow movie theater light style chaser animation."""
@@ -74,25 +89,44 @@ def theaterChaseRainbow(strip, wait_ms=50):
 			for i in range(0, strip.numPixels(), 3):
 				strip.setPixelColor(i+q, 0)
 
+def read_in():
+	try:
+		lines = sys.stdin.readlines()
+		return json.loads(lines[0])
+	except Exception as e:
+		print e
+		return {}
 
-# Main program logic follows:
-if __name__ == '__main__':
-	# Create NeoPixel object with appropriate configuration.
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-	# Intialize the library (must be called once before other functions).
-	strip.begin()
-
-	print ('Press Ctrl-C to quit.')
+def run(strip):
+	p = None
 	while True:
-		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 0, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
-		# Theater chase animations.
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Rainbow animations.
-		rainbow(strip)
-		rainbowCycle(strip)
-		theaterChaseRainbow(strip)
+		command = read_in()
+		if not "mode" in command:
+			continue
+		mode = command["mode"]
+		if p != None:
+			p.terminate()
+		if mode == "clear":
+			p = multiprocessing.Process(target=clear, args=(strip,))
+		elif mode == "colorWipe":
+			args = command["args"]
+			colorWipe(strip, Color(args["color"]["b"], args["color"]["g"], args["color"]["r"]))
+		elif mode == "theaterChase":
+			theaterChase(strip, Color(  0,   0, 127)) 
+		elif mode == "rainbow":
+			rainbow(strip)
+		elif mode =="rainbowCycle":
+			p = multiprocessing.Process(target=rainbowCycle, args=(strip,5,))
+		elif mode == "theaterChaseRainbow":
+			theaterChaseRainbow(strip)
+		else:
+			colorWipe(strip, Color(255, 255, 255))
+
+		p.start()
+
+
+if __name__ == '__main__':
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	strip.begin()
+	print ('Press Ctrl-C to quit.')
+	run(strip)
